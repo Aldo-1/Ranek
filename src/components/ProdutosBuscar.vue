b22ae5f7baa50df2c1512f8c299430709ac5032e
Aldo-1 <aldo.wanderley@souunit.com.br>
2021-06-07 11:04:04 -0300

    products components

---
+++ src/components/ProdutosBuscar.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="produtos-container">
+    <div v-for="produto in produtos" :key="produto.id">
+      <img v-if="produto.fotos" :src="produto.fotos[0].src" :alt="produto.fotos[0].titulo"/>
+      <p class="preco">{{produto.preco}}</p>
+      <h2 class="titulo">{{produto.nome}}</h2>
+      <p>{{produto.descricao}}</p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "produtobuscar",
+  data() {
+    return {
+      produtos: []
+    }
+  },
+  methods:{
+    async getProdutos(){
+      const response = await fetch("http://localhost:3000/produto")
+      const data = await response.json()
+      this.produtos = data
+    }
+  },
+  created(){
+    this.getProdutos()
+  }
+}
+</script>
+
+<style>
+
+</style>
