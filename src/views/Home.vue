994baa2c801d1bf1bfeaef43ef001cfdf2bc7d88
Aldo-1 <aldo.wanderley@souunit.com.br>
2021-06-07 10:51:06 -0300

    adding more components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,30 @@
 <template>
   <div class="home">
-   
+    <h1>👍Compre ou venda👎</h1>
+    <ProdutosLista/>
+    <ProdutosBuscar/>
   </div>
 </template>
 
 <script>
+import ProdutosLista from '@/components/ProdutosLista'
+import ProdutosBuscar from '@/components/ProdutosBuscar'
 
 
 export default {
   name: 'Home',
-  
+  components:{
+    ProdutosLista,
+    ProdutosBuscar
+  }
 }
 </script>
+
+<style scoped>
+h1{
+  font-size: 2rem;
+  color: #87f;
+  margin-top: 40px;
+  text-align: center;
+}
+</style>
