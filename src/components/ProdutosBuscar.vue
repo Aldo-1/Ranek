1a5e620b487be162819829c19439da32d55bfabe
Aldo-1 <aldo.wanderley@souunit.com.br>
2021-06-07 13:44:19 -0300

    creating input style

--- src/components/ProdutosBuscar.vue
+++ src/components/ProdutosBuscar.vue
@@ -1,6 +1,6 @@
 <template>
   <form action="">
-    <input name="busca" id="busca" v-model="busca" type="text">
+    <input name="busca" id="busca" v-model="busca" type="text" placeholder="Buscar ...">
     <input type="submit" id="lupa" value="buscar" @click.prevent="buscarProdutos">
   </form>
 </template>
@@ -21,6 +21,32 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+form{
+  max-width: 600px;
+  margin: 30px auto 60px auto;
+  position: relative;
+}
+#busca{
+  width: 100%;
+  padding: 20px;
+  border: none;
+  border-radius: 4px;
+}
+#busca:focus, #bousca:hover{
+  transform: scale(1.1);
+}
+#lupa{
+  width: 62px;
+  height: 62px;
+  background: url("../assets/search.svg") no-repeat center center;
+  text-indent: -150px;
+  cursor: pointer;
+  border: none;
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  box-shadow: none;
 
+}
 </style>
