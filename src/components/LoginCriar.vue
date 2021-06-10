25b6782f55c063ee12dfb38bcb2331f97b42e78e
Aldo-1 <aldo.wanderley@souunit.com.br>
2021-06-09 21:36:00 -0300

    creating some routes

--- src/components/LoginCriar.vue
+++ src/components/LoginCriar.vue
@@ -1,13 +1,41 @@
 <template>
-  <p>LoginCriar</p>
+  <section>
+    <h2>Crie a sua Conta</h2>
+    <transition mode="out-in">
+      <button class="btn btn-form" v-if="!criar" @click="criar = true">Criar conta</button>
+      <usuario-form v-else>
+        <button class="btn">Criar Usuário</button>
+      </usuario-form>
+    </transition>
+  </section>
 </template>
 
 <script>
+import UsuarioForm from './UsuarioForm.vue'
 export default {
-  name: "LoginCriar"
+  components: { UsuarioForm },
+  name: "LoginCriar",
+  data() {
+    return {
+      criar: false
+    }
+  },
 }
 </script>
 
-<style>
-
+<style scoped>
+  h2{
+    text-align: center;
+    margin-top: 40px;
+    margin-bottom: 10px;
+  }
+  .btn{
+    width: 100%;
+    max-width: 300px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+  .btn-form{
+    max-width: 100%;
+  }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,40 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import api from '@/services.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    login:false,
+    usuario:{
+      id: "",
+      email: "",
+      nome:"",
+      senha:"",
+      cep: "",
+      rua: "",
+      numero: "",
+      bairro: "",
+      cidade: "",
+      estado: ""
+    }
   },
   mutations: {
+    UPDATE_LOGIN(state, payload){
+      state.login = payload
+    },
+    UPDATE_USUARIO(state, payload){
+      state.usuario = payload
+    }
   },
   actions: {
+    async getUsuario(context, payload){
+      const {data} = await api.get(`/usuario/${payload}`)
+      context.commit("UPDATE_USUARIO", data)
+      context.commit("UPDATE_LOGIN", true)
+
+    }
   },
   modules: {
   }
